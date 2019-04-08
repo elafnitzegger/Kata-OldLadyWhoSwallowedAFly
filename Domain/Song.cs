3fd4c7877bad9481a225db815183bdbf9c4a7a0f
Eric Lafnitzegger <eric.lafnitzegger@hotmail.com>
2019-04-08 16:06:12 -0700

    implemented chain and motivation in mainline code

--- Domain/Song.cs
+++ Domain/Song.cs
@@ -45,52 +45,37 @@ namespace Domain
                     return
                         string.Format("There was an old lady who swallowed a {0}.\r\n", "spider") +
                         string.Format("{0}\r\n", "It wriggled and jiggled and tickled inside her.") +
-                        string.Format("She swallowed the {0} to catch the {1}.\r\n", "spider", "fly") +
+                        string.Format("{0}\r\n", Chain(2)) +
                         string.Format("{0}\r\n", "I don't know why she swallowed a fly. Perhaps she'll die!");
                 case 3:
                     return
                         string.Format("There was an old lady who swallowed a {0}.\r\n", "bird") +
                         string.Format("{0}\r\n", "How absurd to swallow a bird!") +
-                        string.Format("She swallowed the {0} to catch the {1}.\r\n", "bird", "spider that wriggled and jiggled and tickled inside her") +
-                        string.Format("She swallowed the {0} to catch the {1}.\r\n", "spider","fly") +
+                        string.Format("{0}\r\n", Chain(3)) +
                         string.Format("{0}\r\n", "I don't know why she swallowed a fly. Perhaps she'll die!");
                 case 4:
                     return 
                         string.Format("There was an old lady who swallowed a {0}.\r\n", "cat") +
                         string.Format("{0}\r\n", "Imagine that! She swallowed a cat!") +
-                        string.Format("She swallowed the {0} to catch the {1}.\r\n", "cat", "bird") +
-                        string.Format("She swallowed the {0} to catch the {1}.\r\n", "bird", "spider that wriggled and jiggled and tickled inside her") +
-                        string.Format("She swallowed the {0} to catch the {1}.\r\n", "spider", "fly") +
+                        string.Format("{0}\r\n", Chain(4)) +
                         string.Format("{0}\r\n", "I don't know why she swallowed a fly. Perhaps she'll die!");
                 case 5:
                     return
                         string.Format("There was an old lady who swallowed a {0}.\r\n", "dog") +
                         string.Format("{0}\r\n", "What a hog, to swallow a dog!") +
-                        string.Format("She swallowed the {0} to catch the {1}.\r\n", "dog", "cat") +
-                        string.Format("She swallowed the {0} to catch the {1}.\r\n", "cat", "bird") +
-                        string.Format("She swallowed the {0} to catch the {1}.\r\n", "bird", "spider that wriggled and jiggled and tickled inside her") +
-                        string.Format("She swallowed the {0} to catch the {1}.\r\n", "spider", "fly") +
+                        string.Format("{0}\r\n", Chain(5)) +
                         string.Format("{0}\r\n", "I don't know why she swallowed a fly. Perhaps she'll die!");
                 case 6:
                     return
                         string.Format("There was an old lady who swallowed a {0}.\r\n", "goat") +
                         string.Format("{0}\r\n", "She just opened her throat and swallowed a goat!") +
-                        string.Format("She swallowed the {0} to catch the {1}.\r\n", "goat", "dog") +
-                        string.Format("She swallowed the {0} to catch the {1}.\r\n", "dog", "cat") +
-                        string.Format("She swallowed the {0} to catch the {1}.\r\n", "cat", "bird") +
-                        string.Format("She swallowed the {0} to catch the {1}.\r\n", "bird", "spider that wriggled and jiggled and tickled inside her") +
-                        string.Format("She swallowed the {0} to catch the {1}.\r\n", "spider", "fly") +
+                        string.Format("{0}\r\n", Chain(6)) +
                         string.Format("{0}\r\n", "I don't know why she swallowed a fly. Perhaps she'll die!");
                 case 7:
                     return
                         string.Format("There was an old lady who swallowed a {0}.\r\n", "cow") +
                         string.Format("{0}\r\n", "I don't know how she swallowed a cow!") +
-                        string.Format("She swallowed the {0} to catch the {1}.\r\n", "cow", "goat") +
-                        string.Format("She swallowed the {0} to catch the {1}.\r\n", "goat", "dog") +
-                        string.Format("She swallowed the {0} to catch the {1}.\r\n", "dog", "cat") +
-                        string.Format("She swallowed the {0} to catch the {1}.\r\n", "cat", "bird") +
-                        string.Format("She swallowed the {0} to catch the {1}.\r\n", "bird", "spider that wriggled and jiggled and tickled inside her") +
-                        string.Format("She swallowed the {0} to catch the {1}.\r\n", "spider", "fly") +
+                        string.Format("{0}\r\n", Chain(7)) +
                         string.Format("{0}\r\n", "I don't know why she swallowed a fly. Perhaps she'll die!");
                 case 8:
                     return
